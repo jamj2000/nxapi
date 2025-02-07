b33a82486708c853da357c106a5a40f90201e33e
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-02-07 21:38:53 +0100

    await params

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,8 +1,10 @@
 import getUsers from "@/lib/users";
 
-export function GET(request, { params }) {
+export async function GET(request, { params }) {
+    const {id} = await params
+    
     let users = getUsers();
-    let usuario = users.find(user => user.id == params.id)
+    let usuario = users.find(user => user.id == id)
 
     return Response.json(usuario)
 }
@@ -13,22 +15,26 @@ export async function PUT(request, { params }) {
     if (content != 'application/json')
         return Response.json({ message: 'Debes proporcionar datos JSON' })
 
+    const {id} = await params
+
     let users = getUsers();
     // Obtenemos posición    
-    const pos = users.findIndex(user => user.id == params.id)
+    const pos = users.findIndex(user => user.id == id)
 
     // Modificamos usuario
     const newUser = await request.json()
-    users.splice(pos, 1, { id: Number(params.id), ...newUser })
+    users.splice(pos, 1, { id: Number(id), ...newUser })
 
     return Response.json(users)
 }
 
 
-export function DELETE(request, { params }) {
+export async function DELETE(request, { params }) {
+    const {id} = await params
+    
     let users = getUsers();
     // Obtenemos posición    
-    const pos = users.findIndex(user => user.id == Number(params.id))
+    const pos = users.findIndex(user => user.id == Number(id))
 
     // Eliminamos usuario
     if (pos != -1)  // Si es encontrado
